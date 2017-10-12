9516d9da38842966451b96d14daf5a9ab0c7e67e
AsmatUllah Muradi <Asmat@AsmatUllahs-MacBook-Pro.local>
2017-10-11 22:41:58 -0400

    added a controller and edited route

---
+++ app/Http/Controllers/BillSplitController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class BillSplitController extends Controller
+{
+    public function index()
+    {
+      return 'Show form to collect info from user....';
+    }
+
+    public function calculateTip()
+    {
+      return 'In this method we will calculate tip...';
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,6 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'BillSplitController@index');
+
+Route::get('/calculate-tip', 'BillSplitController@calculateTip');
